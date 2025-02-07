91630801654993bca6d929f4791aac63b3ce1db1
Bhavik94290 <bhaviknakarani693@gmail.com>
2025-02-07 16:20:29 +0530

    init

---
+++ src/main/java/com/grownited/controller/sessionController.java
@@ -0,0 +1,22 @@
+package com.grownited.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class sessionController {
+	
+	@GetMapping ("signup")
+	public String signup() {
+		return "Signup";
+		
+	}
+
+
+	@GetMapping("login")
+	public String login() {
+		
+		return "Login";
+	}
+	
+}
+
